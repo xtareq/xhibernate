d7b860149d7cf85f2a752bb540a85170d15514f8
xtareq <xtrinsic96@gmail.com>
2018-10-24 06:45:25 +0100

    setup changed

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import javax.persistence.*;
 
+@Entity
+@Table(name="banks_table")
 public class Bank {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(generator="incrementor")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+        //entitmanagerfactory by persistance
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
+
+        //Instancite client
+
+        Client client = new Client();
+        client.setId(2);
+        client.setName("Tareq");
+
+        //new Bank
+
+        Bank bank = new Bank();
+        bank.setName("DBBL");
+
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        //begain transection
+
+        entityManager.getTransaction().begin();
+
+        //persist bank and client
+
+        entityManager.persist(bank);
+        entityManager.persist(client);
+
+        //commiting transaction
+
+        entityManager.getTransaction().commit();
+
+        entityManagerFactory.close();
+    }
 }
